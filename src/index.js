bd5d2d9a55a18e37fde2b2f7e6fa7cc04310c299
alina-kam <alinakamenets08@gmail.com>
2022-08-18 11:20:05 +0300

    feat: add solution except for same brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const OPEN_BRACKETS = [];
+  for (entry of bracketsConfig){
+    OPEN_BRACKETS.push(entry[0]);
+  }
+
+  const BRACKETS_PAIRS = Object.fromEntries(bracketsConfig);
+
+  let bracketsStack = [];
+
+  for (let i=0; i<str.length; i++){
+    if (OPEN_BRACKETS.includes(str[i])){
+      bracketsStack.push(str[i])
+    }
+    else if (str[i] === BRACKETS_PAIRS[bracketsStack[bracketsStack.length - 1]]){
+      bracketsStack.pop();
+    }
+    else return false;
+  }
+  if (bracketsStack.length) return false;
+  return true;
 }
+
+console.log(module.exports('()', [['(', ')']]));
